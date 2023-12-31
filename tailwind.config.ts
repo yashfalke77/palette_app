cbfd859ae8c22e339e8f607ce125955ed44b2ecb
Yash Falke <yashfalke77@gmail.com>
2023-12-31 19:48:13 +0530

    styles initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,94 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  prefix: '',
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
       },
     },
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
+      fontSize: {
+        base: '1rem',
+      },
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        'black': '#000000',
+        'white': '#ffffff',
+        primary: {
+          '200': '#0066ff',
+          '400': '#005ce6',
+          '600': '#0052cc',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      screens: {
+        'sm': '600px',
+        'md': '960px',
+        'lg': '1280px',
+        'xl': '1440px',
+        'max-xl': { 'max': '1440px' },
+        'max-lg': { 'max': '1279px' },
+        'max-md': { 'max': '960px' },
+        'max-sm': { 'max': '600px' },
+      },
+      'home': "url('../../../public/Images/home.png')",
+    },
   },
-  plugins: [],
+  // eslint-disable-next-line unicorn/prefer-module
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
