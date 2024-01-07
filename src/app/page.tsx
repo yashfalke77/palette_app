d191d4758bf97198f647d92d62b7317eef03cc44
Yash Falke <yashfalke77@gmail.com>
2024-01-07 20:16:05 +0530

    added hero and features section on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Features from "@/components/Features/Features";
+import HeroSection from "@/components/HeroSection/HeroSection";
 import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main >
       <Navbar />
+      <HeroSection />
+      <Features />
     </main>
   );
 }
